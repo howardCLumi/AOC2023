eb9e9f23f77e6f82a65ebc23807c759a153e9a19
howardCLumi <102765211+howardCLumi@users.noreply.github.com>
2023-12-02 01:05:06 -0600

    end of day 1

--- dayOne/partOne/script.mjs
+++ dayOne/partOne/script.mjs
@@ -1,11 +1,6 @@
-import { fileURLToPath } from "node:url";
 import { readFile } from "node:fs/promises";
-import path from "node:path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const filePath = path.resolve(__dirname, "../input.txt");
-const input = await readFile(filePath, "utf-8");
+const input = await readFile("../input.txt", "utf-8");
 const array = input.split(/\r?\n/).map((line) => line.replace(/\r/g, ""));
 let sum = 0;
 
